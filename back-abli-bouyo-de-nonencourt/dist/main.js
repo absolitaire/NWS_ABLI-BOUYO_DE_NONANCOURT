7e3eb7311765e1ddbb216126a9c68c9d4374f65a
ABLI-BOUYO Alexis <ablibouy1u@etu.univ-lorraine.fr>
2019-11-15 12:31:34 +0100

    fichier de configuration et ajout des dépendances

--- back-abli-bouyo-de-nonencourt/dist/main.js
+++ back-abli-bouyo-de-nonencourt/dist/main.js
@@ -2,9 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+const platform_fastify_1 = require("@nestjs/platform-fastify");
+const common_1 = require("@nestjs/common");
+const Config = require("config");
+async function bootstrap(config) {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_fastify_1.FastifyAdapter({ logger: true }));
+    await app.listen(config.port, config.host);
+    common_1.Logger.log(`Application served at http://${config.host}:${config.port}`, 'bootstrap');
 }
-bootstrap();
+bootstrap(Config.get('server'));
 //# sourceMappingURL=main.js.map
--- back-abli-bouyo-de-nonencourt/src/main.ts
+++ back-abli-bouyo-de-nonencourt/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { Logger } from '@nestjs/common';
+import { AppConfig } from './interfaces/app-config.interface';
+import * as Config from 'config';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap(config: AppConfig) {
+  const app = await NestFactory.create(AppModule,
+    new FastifyAdapter({logger: true}));
+
+  // launch server
+  await app.listen(config.port, config.host);
+  Logger.log(`Application served at http://${config.host}:${config.port}`, 'bootstrap');
+
 }
-bootstrap();
+bootstrap(Config.get<AppConfig>('server'));
